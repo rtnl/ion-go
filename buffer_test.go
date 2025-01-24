c9c5824fb15cc00fed04f2c3340a40cb376133e9
snavy <1337snavy@gmail.com>
2025-01-24 21:00:00 +0100

    add buffer read write tests

---
+++ buffer_test.go
@@ -0,0 +1,52 @@
+package ion
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewBuffer(t *testing.T) {
+	buffer := NewBuffer()
+	assert.NotNil(t, buffer)
+	assert.NotNil(t, buffer.inner)
+}
+
+func TestBuffer_Write(t *testing.T) {
+	var (
+		size int
+		err  error
+	)
+
+	buffer := NewBuffer()
+
+	src := []byte("In the beginning God created the heaven and the earth.")
+
+	size, err = buffer.Write(src)
+	assert.Nil(t, err)
+	assert.Equal(t, len(src), size)
+
+	dst := buffer.Consume()
+	assert.NotNil(t, dst)
+	assert.Equal(t, src, dst)
+}
+
+func TestBuffer_Read(t *testing.T) {
+	var (
+		size int
+		err  error
+	)
+
+	buffer := NewBuffer()
+
+	src := []byte("In the beginning God created the heaven and the earth.")
+	dst := make([]byte, len(src))
+
+	size, err = buffer.Write(src)
+	assert.Nil(t, err)
+	assert.Equal(t, len(src), size)
+
+	size, err = buffer.Read(dst)
+	assert.Nil(t, err)
+	assert.Equal(t, len(src), size)
+	assert.Equal(t, src, dst[:size])
+}
