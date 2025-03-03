55a58c473c4d0d2d9ee9580be813559fe118e1cc
snavy <1337snavy@gmail.com>
2025-03-03 21:46:20 +0100

    buffer peek io methods

--- buffer_io.go
+++ buffer_io.go
@@ -61,6 +61,23 @@ func (b *Buffer) ReadObject(kind ObjectKind) (value any, err error) {
 	}
 }
 
+func (b *Buffer) PeekObject(kind ObjectKind) (value any, err error) {
+	switch kind {
+	case U0:
+		return b.PeekU0()
+	case U8:
+		return b.PeekU8()
+	case U16:
+		return b.PeekU16()
+	case U32:
+		return b.PeekU32()
+	case U64:
+		return b.PeekU64()
+	default:
+		return nil, fmt.Errorf("unimplemented object type: %v", reflect.TypeOf(value))
+	}
+}
+
 func (b *Buffer) WriteU0() (err error) {
 	if b.inner == nil {
 		err = fmt.Errorf("buffer is nil")
@@ -450,6 +467,234 @@ func (b *Buffer) ReadList(kinds []ObjectKind) (value *list.List, err error) {
 	}
 
 	err = b.ReadListClose()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (b *Buffer) PeekU0() (value any, err error) {
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+		return
+	}
+
+	err = Check(C.ion_buffer_io_peek_u0(b.inner))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (b *Buffer) PeekU8() (value uint8, err error) {
+	var (
+		v C.uint8_t
+	)
+
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+		return
+	}
+
+	err = Check(C.ion_buffer_io_peek_u8(b.inner, &v))
+	if err != nil {
+		return
+	}
+
+	value = uint8(v)
+
+	return
+}
+
+func (b *Buffer) PeekU16() (value uint16, err error) {
+	var (
+		v C.uint16_t
+	)
+
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+		return
+	}
+
+	err = Check(C.ion_buffer_io_peek_u16(b.inner, &v))
+	if err != nil {
+		return
+	}
+
+	value = uint16(v)
+
+	return
+}
+
+func (b *Buffer) PeekU32() (value uint32, err error) {
+	var (
+		v C.uint32_t
+	)
+
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+		return
+	}
+
+	err = Check(C.ion_buffer_io_peek_u32(b.inner, &v))
+	if err != nil {
+		return
+	}
+
+	value = uint32(v)
+
+	return
+}
+
+func (b *Buffer) PeekU64() (value uint64, err error) {
+	var (
+		v C.uint64_t
+	)
+
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+		return
+	}
+
+	err = Check(C.ion_buffer_io_peek_u64(b.inner, &v))
+	if err != nil {
+		return
+	}
+
+	value = uint64(v)
+
+	return
+}
+
+func (b *Buffer) PeekArrayOpen() (kind ObjectKind, length int, err error) {
+	var (
+		k C.t_ion_object_kind
+		l C.uint8_t
+	)
+
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+		return
+	}
+
+	err = Check(C.ion_buffer_io_peek_arr_open(b.inner, &k, &l))
+	if err != nil {
+		return
+	}
+
+	kind = ObjectKind(k)
+	length = int(l)
+
+	return
+}
+
+func (b *Buffer) PeekArrayClose() (err error) {
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+		return
+	}
+
+	err = Check(C.ion_buffer_io_peek_arr_close(b.inner))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func BufferPeekArray[T any](b *Buffer, kind ObjectKind) (value []T, err error) {
+	var (
+		length  int
+		valKind ObjectKind
+		valRaw  any
+	)
+
+	valKind, length, err = b.PeekArrayOpen()
+	if err != nil {
+		return
+	}
+
+	if valKind != kind {
+		err = fmt.Errorf("inner type does not match: %v %v", kind, valKind)
+		return
+	}
+
+	value = make([]T, length)
+	for x := range length {
+		valRaw, err = b.PeekObject(kind)
+		if err != nil {
+			return
+		}
+
+		value[x] = valRaw.(T)
+	}
+
+	err = b.PeekArrayClose()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (b *Buffer) PeekListOpen() (length int, err error) {
+	var (
+		l C.uint8_t
+	)
+
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+		return
+	}
+
+	err = Check(C.ion_buffer_io_peek_list_open(b.inner, &l))
+	if err != nil {
+		return
+	}
+
+	length = int(l)
+
+	return
+}
+
+func (b *Buffer) PeekListClose() (err error) {
+	if b.inner == nil {
+		err = fmt.Errorf("buffer is nil")
+	}
+
+	err = Check(C.ion_buffer_io_peek_list_close(b.inner))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (b *Buffer) PeekList(kinds []ObjectKind) (value *list.List, err error) {
+	var (
+		length int
+		item   any
+	)
+
+	length, err = b.ReadListOpen()
+	if err != nil {
+		return
+	}
+
+	value = list.New()
+
+	for x := range length {
+		item, err = b.PeekObject(kinds[x])
+		if err != nil {
+			return
+		}
+
+		value.PushBack(item)
+	}
+
+	err = b.PeekListClose()
 	if err != nil {
 		return
 	}
