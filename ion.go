522c4bc1a98c055c977bb0ee0f93c448ffeffe5b
snavy <1337snavy@gmail.com>
2025-03-03 21:01:48 +0100

    test script

--- ion.go
+++ ion.go
@@ -1,12 +1,8 @@
 package ion
 
 // #cgo CFLAGS: -I/usr/local/include
-// #cgo LDFLAGS: -L/usr/local/include -lion -Wl,-rpath,/usr/local/include
+// #cgo LDFLAGS: -L/usr/local/lib -lion -Wl,-rpath,/usr/local/lib
 // #include "ion.h"
 import (
 	"C"
 )
-
-func init() {
-
-}
